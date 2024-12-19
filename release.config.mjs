3969b32019e36e0de9f543c0a011801fd06e7d17
Daniel Núñez <daniel.portonunez@gmail..com>
2024-12-19 13:03:18 -0300

    ci(release): add prerelease setting test

--- release.config.mjs
+++ release.config.mjs
@@ -2,18 +2,11 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: [
-    { name: "main" }, // Stable releases
-    { name: "alpha", channel: "alpha", prerelease: "alpha" }, // Development pre-releases
-  ],
+  branches: [{ name: "main", prerelease: "alpha" }],
   plugins: [
     "@semantic-release/commit-analyzer", // Analyze commits
     "@semantic-release/release-notes-generator", // Generate release notes
     "@semantic-release/github", // Publish GitHub releases
   ],
   preset: "angular", // Conventional Commit standard
-  releaseRules: [
-    { type: "breaking", release: "minor" }, // Treat breaking changes as minor in 0.x.y
-    { type: "feat", release: "patch" }, // Treat features as patch in 0.x.y
-  ],
 };
