265c08eb5614e9eae980b9395a733665923dc90a
Daniel Núñez <daniel.portonunez@gmail..com>
2024-12-19 14:12:07 -0300

    ci(release): pub first release

--- release.config.mjs
+++ release.config.mjs
@@ -10,18 +10,7 @@ export default {
     },
   ],
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        releaseRules: [
-          { breaking: true, release: "minor" }, // Breaking changes are minor during alpha
-          { revert: true, release: "patch" },
-          { type: "feat", release: "minor" },
-          { type: "fix", release: "patch" },
-          { type: "perf", release: "patch" },
-        ],
-      },
-    ],
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
   ],
